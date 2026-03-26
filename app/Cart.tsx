db540421081a1a121f42da2ab468452d1c87bc48
MatheusHen <matheushenriquesouzasantos2@gmail.com>
2026-03-26 15:52:04 -0300

    feat: adding shopping cart, with all itens and total, and adding render counting

---
+++ app/Cart.tsx
@@ -0,0 +1,74 @@
+import { useContext } from "react";
+import { FlatList, StyleSheet, Text, useWindowDimensions, View } from "react-native";
+import { CartContext } from "./context/CartContext";
+
+
+export default function Cart(){
+    const {width}=useWindowDimensions();
+    const isMobile=width<768;
+    const cartContext=useContext(CartContext);
+    const itens=cartContext?.itens;
+    const total=cartContext?.cartTotal;
+    
+    return(
+        <View>
+            <FlatList 
+            data={itens}
+            keyExtractor={(item)=>item.id}
+            renderItem={({item})=>
+                <View style={styles.container}>
+                <Text style={styles.textStyle}>{`${item.name} - ${item.preco}R$`}</Text>
+                </View>    
+            }/>
+            {itens?.length==0 && 
+            <View style={styles.viewEmptyContent}>
+                <Text style={styles.textStyle} >Carrinho Vazio</Text>    
+            </View>}
+            <View style={[styles.totalView,{
+                left: isMobile ? "45%" : "82%",
+                top: isMobile ? 720 : 800
+            }]}>
+                <Text style={styles.textStyle}>{`Total: ${total} R$`}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles=StyleSheet.create({
+  container:{
+    backgroundColor: "#763dd9",
+    width: 380,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 25,
+    height: 80,
+    marginLeft: 10,
+    borderRadius: 12
+  },
+  textStyle: {
+    fontSize: 27,
+    marginLeft: 10
+  },
+  totalView:{
+    backgroundColor: "#763dd9",
+    width: 200,
+    height: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    borderRadius: 15,
+  },
+  viewEmptyContent: {
+    backgroundColor: "#763dd9",
+    width: 200,
+    height: 50,
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 10,
+    marginLeft: 10,
+    borderRadius: 15
+  }
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { useCallback, useContext } from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useCallback, useContext, useRef } from "react";
+import { FlatList, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
 import { CartContext, Item } from "./context/CartContext";
+
 export const products : Item[]= [
  { id: '1', name: 'Notebook', preco: 3000 },
  { id: '2', name: 'Mouse', preco: 100 },
@@ -10,10 +11,17 @@ export const products : Item[]= [
 ];
 
 export default function Index() {
+  const {width}=useWindowDimensions();
+  const isMobile=width<768;
   const contexto = useContext(CartContext);
   if(contexto==null){
     throw new Error("Use context needs a provider")
   }
+
+  const renderCount=useRef(0);
+
+  renderCount.current+=1
+
   const {addToCart}=contexto
 
   const lookProductDetails=useCallback(()=>{
@@ -37,9 +45,18 @@ export default function Index() {
             </TouchableOpacity>
           </View>    
         </View> }/>
-        <View style={styles.viewTotal}>
-
+        <View style={[styles.viewRenderizationCount,{
+            left: isMobile ? "20%" : "73%",
+            top: isMobile ? 735 : 815,
+        }]}>
+          <Text style={styles.textRenderizationCount}>{`Render Count: ${renderCount.current}`}</Text>
         </View>
+        <Link href={"./Cart"} style={[styles.linkShoppingCart,{
+          left: isMobile ? "80%" : "93%",
+          top: isMobile ? 720 : 800,
+        }]}>
+          <FontAwesome5 size={30} name="shopping-cart"/>
+        </Link>
       </View>    
   );
 }
@@ -73,7 +90,27 @@ const styles=StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center"
   },
-  viewTotal:{
-
+  viewRenderizationCount:{
+    position: "absolute",
+    backgroundColor: "#763dd9",
+    borderRadius: 15,
+    width: 220,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  textRenderizationCount: {
+    fontSize: 27
+  },
+  linkShoppingCart: {
+    position: "absolute",
+    backgroundColor: "#763dd9",
+    borderRadius: "50%",
+    width: 65,
+    height: 65,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
